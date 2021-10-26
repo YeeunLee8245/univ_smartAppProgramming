f26d94f3986f89e7f49ae731ae34eda5ccaa3aa8
Yeeun Lee <77772720+YeeunLee8245@users.noreply.github.com>
2021-10-26 14:22:05 +0900

    Add files via upload

---
+++ week8-3/imageActivity2.kt
@@ -0,0 +1,11 @@
+package kr.co.yeaeun.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class imageActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image2)
+    }
+}
