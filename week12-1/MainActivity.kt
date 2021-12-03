53637b727bd2f3f732713d25c273d7953ef8b12d
Yeeun Lee <77772720+YeeunLee8245@users.noreply.github.com>
2021-12-03 22:44:47 +0900

    Update MainActivity.kt

--- week12-1/MainActivity.kt
+++ week12-1/MainActivity.kt
@@ -2,10 +2,11 @@ package kr.co.yeaeun.mysonglist
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
-// dependency로 volley 추가해주었기 때문에 사용 가능
+// dependency로 volley 추가해주었기 때문에 사용 가능 또한 volley를 사용하려면 Manifest에 Internet도 허용해줘야 함
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 
@@ -21,9 +22,10 @@ class MainActivity : AppCompatActivity() {
 
         val url = "https://bwoh.tistory.com"
 
+        // 요청과 핸들러 구체화
         val stringRequest = StringRequest(
             Request.Method.GET, url,
-            Response.Listener<String> { response ->
+            { response ->
                 //textView.text = response
 //                webView.settings.defaultTextEncodingName = "UTF-8"
 //                webView.loadData(response, "text/html;charset=UTF-8", null)
@@ -32,6 +34,7 @@ class MainActivity : AppCompatActivity() {
                 val ogDesc = "\"og:description\""
                 val ogImage = "\"og:image\""
 
+                // reponse: 웹 html
                 val resultTitle = findContent(response,ogTitle,0)
                 val resultDesc = findContent(response,ogDesc,0)
                 val resultImage = findContent(response,ogImage,0)
@@ -48,31 +51,33 @@ class MainActivity : AppCompatActivity() {
                             <img src="$resultImage"/>
                         </body>
                     </html>
-                """.trimIndent()
+                """.trimIndent() // 들여쓰기 제거(없어도 됨)
 
                 webView.settings.defaultTextEncodingName = "UTF-8"
+                // webView 작성
                 webView.loadData(html, "text/html;charset=UTF-8", null)
 
 //                textView.text = findContent(response,ogTitle,0)
 //                textView.text = findContent(response,ogDesc,0)
 
             },
-            Response.ErrorListener {
+            {
                 textView.text = "Error: $it"
             }
         )
+        // 요청 큐에 삽입
         queue.add(stringRequest)
     }
     fun findContent(target: String, meta: String, startIndex:Int): String{
         val content = "content=\""
         val close = "\" >"
 
-        var start = target.indexOf(meta)
-        start = target.indexOf(content, start)
-        val end = target.indexOf(close, start)
+        var start = target.indexOf(meta) // target(reponse)에서 meta("og:title") 데이터의 시작(") index
+        start = target.indexOf(content, start)  // target(reponse)에서 "(og:..)이후부터 content의 시작 index
+        val end = target.indexOf(close, start)  // target(reponse)에서 start이후부터 close의 시작 index
 
-        // content(content=") 다음부터 출력 시작
-        return target.substring(start + content.length..end - 1)
+        // content(content=") 다음부터 출력 시작 =>
+        return target.substring((start + content.length)..end - 1)
     }
 
 }
